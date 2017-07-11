a5341271fcc0a9f7b1a7ae5c625101162ee8da19
Rihards Baranovskis <multiks2200@gmail.com>
2017-07-11 14:33:44 +0100

    added embarrassingly simple data batching parallesition

--- models/model.py
+++ models/model.py
@@ -3,24 +3,35 @@ import tensorflow as tf
 from functools import reduce
 import matplotlib.pyplot as plt
 from tensorflow.python import debug as tf_debug
+from numpy.random import RandomState
+import random
+import time
+import threading 
+from tensorflow.python.client import timeline
 
 #model flags
 tf.flags.DEFINE_boolean("debug", False, "weather run in a dubg mode")
+tf.flags.DEFINE_integer("seed", round(random.random()*100000), "the global simulation seed for np and tf")
 datatype = tf.float64
 FLAGS = tf.flags.FLAGS
 
+
+#set random seed
+tf.set_random_seed(FLAGS.seed)
+
 #configuraion constants
-num_epochs = 1000000
-state_size = 4
+total_num_epochs = 1000000
+iters_per_epoch = 1
+num_epochs = total_num_epochs // iters_per_epoch
+state_size = 50
 num_of_operations = 3
-max_output_ops = 1
-num_batches = 100
+max_output_ops = 2
 num_features = 3
-num_samples = 10
-max_num_features = 16
-max_num_outputs = 16
-batch_size = num_samples
+num_samples = 50000
+batch_size  = 100
+num_batches = num_samples//batch_size
 param_init = 0.1
+learning_rate = 0.005
 #sample gen functions
 def np_add(vec):
     return reduce((lambda x, y: x + y),vec)
@@ -31,16 +42,16 @@ def np_mult(vec):
 def np_stall(vec):
     return vec
 
-def samples_generator(fn, shape, rng, seed=None):
+def samples_generator(fn, shape, rng, seed):
     '''
     Generate random samples for the model:
     @fn - function to be applied on the input features to get the ouput
     @shape - shape of the features matrix (num_samples, num_features)
     @rng - range of the input features to be generated within (a,b)
-    @seed  - generation seed
     Outputs a tuple of input and output features matrix
     '''
-    x = (rng[1] - rng[0]) * np.random.random_sample(shape) + rng[0]
+    prng = RandomState(seed)
+    x = (rng[1] - rng[0]) * prng.random_sample(shape) + rng[0]
     y = np.apply_along_axis(fn, 1, x).reshape((shape[0],-1))
     z = np.zeros((shape[0],shape[1] - y.shape[1]))
     y = np.concatenate((y, z), axis=1)
@@ -82,6 +93,11 @@ def run_forward_pass(mode="train"):
 
     outputs = []
 
+    softmaxes = []
+    
+    #printtf = tf.Print(output, [output], message="Strated cycle")
+    #output = tf.reshape( printtf, [batch_size, -1], name = "dummu_rehap")
+    
     for timestep in range(max_output_ops):
         print("timestep " + str(timestep))
         current_input = output
@@ -156,7 +172,10 @@ def run_forward_pass(mode="train"):
         output = tf.add(add_final, mult_final, name="add_final_op_mult_add")
         output =  tf.add(output, stall_final, name="add_final_op_stall")
         outputs.append(output)
-    return output, current_state, softmax
+        softmaxes.append(softmax)
+    #printtf = tf.Print(output, [output], message="Finished cycle")
+    #output = tf.reshape( printtf, [batch_size, -1], name = "dummu_rehap")
+    return output, current_state, softmax, outputs, softmaxes
 
 #cost function
 def calc_loss(output):
@@ -166,23 +185,44 @@ def calc_loss(output):
     total_loss = tf.reduce_sum(math_error, name="red_total_loss")
     return total_loss, math_error
 
-output, current_state, softmax = run_forward_pass(mode = "train")
+output, current_state, softmax, outputs, softmaxes = run_forward_pass(mode = "train")
 total_loss, math_error = calc_loss(output)
-
-output_test, current_state_test, softmax_test = run_forward_pass(mode = "test")
-total_loss_test, math_error_test = calc_loss(output_test)
+max_output_ops
+#output_test, current_state_test, softmax_test, outputs_test, softmaxes_test = run_forward_pass(mode = "test")
+#total_loss_test, math_error_test = calc_loss(output_test)
 
 grads = tf.gradients(total_loss, [W,b,W2,b2], name="comp_gradients")
-train_step = tf.train.AdamOptimizer(0.1, epsilon=1e-6 ,name="AdamOpt").apply_gradients(zip(grads, [W,b,W2,b2]), name="min_loss")
+train_step = tf.train.AdamOptimizer(learning_rate, epsilon=1e-6 ,name="AdamOpt").apply_gradients(zip(grads, [W,b,W2,b2]), name="min_loss")
 print("grads are")
 print(grads)
 
-#numpy printpoints
-np.set_printoptions(precision=3)
-
+#pre training setting
+np.set_printoptions(precision=3, suppress=True)
+#train_fn = np_add
+train_fn = np_mult
+#train_fn = np_stall
+x,y = samples_generator(train_fn, (num_samples, num_features) , (-100, 100), FLAGS.seed)
 #model training
-with tf.Session() as sess:
-    
+
+
+def launch_batch(run_options, run_metadata, coord, sess, _current_state, batchX, batchY, loss_list_train):                
+    _total_loss, _train_step, _current_state, _output, _grads, _softmaxes, _math_error = sess.run(
+                    [total_loss, train_step, current_state, output, grads, softmaxes, math_error],
+                    feed_dict={
+                        init_state:_current_state,
+                        batchX_placeholder:batchX,
+                        batchY_placeholder:batchY
+                    }, options=run_options, run_metadata=run_metadata)
+    loss_list_train.append(_total_loss)
+    #coord.request_stop()
+
+#Enable jit
+config = tf.ConfigProto()
+config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
+
+with tf.Session(config=config) as sess:
+    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    run_metadata = tf.RunMetadata()
     ##enable debugger if necessary
     if (FLAGS.debug):
         print("Running in a debug mode")
@@ -192,43 +232,63 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     #plt.ion()
     #plt.figure()
-    #plt.show()
-    loss_list = []
-
+    #plt.show() 
+    globalstartTime = time.time()
     for epoch_idx in range(num_epochs):
-        _current_state = np.zeros((batch_size, state_size))
-        x,y = samples_generator(np_stall, (num_samples, num_features) , (-100, 100))
-        #print("\r New data, epoch", epoch_idx)
+        startTime = time.time()
+        loss_list_train = []
+        loss_list_test  = []
+        
+        for _iter in range(iters_per_epoch):
+            _current_state = np.zeros((batch_size, state_size))
+            #print("\r New data, epoch", epoch_idx)
+            coord = tf.train.Coordinator()
+            threads = []
+            for batch_idx in range(num_batches):
+                start_idx = batch_size * batch_idx
+                end_idx   = batch_size * batch_idx + batch_size
 
-        for batch_idx in range(num_batches):
+                batchX = x[start_idx:end_idx]
+                batchY = y[start_idx:end_idx]
+                if _iter == iters_per_epoch - 1:
+                    launch_batch(run_options, run_metadata, coord, sess, _current_state, batchX, batchY, loss_list_train)
+                    #threads.append(threading.Thread(target=launch_batch, args=(run_options, run_metadata, coord, sess, _current_state, batchX, batchY, loss_list_train)))
+                else: 
+                    launch_batch(run_options, run_metadata, coord, sess, _current_state, batchX, batchY, [])
+                    #threads.append(threading.Thread(target=launch_batch, args=(run_options, run_metadata, coord, sess, _current_state, batchX, batchY, [])))
+                #_total_loss_test, _softmaxes_test, _output_test, _math_error_test = sess.run([total_loss_test, softmaxes_test, output_test, math_error_test], feed_dict={init_state:_current_state, batchX_placeholder:batchX, batchY_placeholder:batchY})  
 
-            _total_loss, _train_step, _current_state, _output, _grads, _softmax, _math_error = sess.run(
-                    [total_loss, train_step, current_state, output, grads, softmax, math_error],
-                    feed_dict={
-                        init_state:_current_state,
-                        batchX_placeholder:x,
-                        batchY_placeholder:y
-                    })
-            loss_list.append(_total_loss)
-            
-            
+                #loss_list_test.append(_total_loss_test)
+        '''
+        for t in threads:
+          t.start()
+        coord.join(threads)    
+        '''
+        print("")
         #harmax test
-        _total_loss_test, _softmax_test, _output_test, _math_error_test = sess.run([total_loss_test, softmax_test, output_test, math_error_test], feed_dict={init_state:_current_state, batchX_placeholder:x, batchY_placeholder:y})
+        '''
         print(" ")
-
         print("output_train\t\t\t\t\toutput_test")
         print(np.column_stack((_output, _output_test)))
         print("x\t\t\\t\t\t\t\y")
-        print(np.column_stack((x, y)))
-        print("softmax_train\t\t\t\t\softmax_test")
-        print(np.column_stack((_softmax, _softmax_test)))
+        print(np.column_stack((batchX, batchY)))
+        print("softmaxes_train\t\t\t\t\softmaxes_test")
+        print(np.column_stack((_softmaxes, _softmaxes_test)))
         print("mat_error_train\t\t\t\t\math_error_test")
         print(np.column_stack((_math_error, _math_error_test)))
-        print("Epoch",epoch_idx, " Loss\t", _total_loss , "    ")
-        print("Harmax test\t", _total_loss_test)
+        '''
+        print("Epoch",epoch_idx, " Loss\t", reduce(lambda x, y: x+y, loss_list_train))
+        #print("Harmax test\t", reduce(lambda x, y: x+y, loss_list_test))
+        print("Epoch time: ", ((time.time() - startTime) % 60), " Global Time: ",  ((time.time() - globalstartTime) % 60) )
+        print("func: ", train_fn.__name__, "max_ops: ", max_output_ops, "sim_seed", FLAGS.seed)
         #print("grads[0] - W", _grads[0][0])
         #print("grads[1] - b", _grads[1][0])
         #print("grads[2] - W2", _grads[2][0])
         #print("grads[3] - b2", _grads[3][0])
         #print("W", W.eval())
         #print("w2" , W2.eval())
+        #record execution timeline
+        tl = timeline.Timeline(run_metadata.step_stats)
+        ctf = tl.generate_chrome_trace_format()
+        with open('./timeline.json', 'w') as f:
+            f.write(ctf)
