4441e94c0fb0c3997bd4e8830de2bc4a80d6e326
Rihards Baranovskis <multiks2200@gmail.com>
2017-08-14 09:45:45 +0100

    added mising ordered dict to gridserach

--- models/gridsearch.py
+++ models/gridsearch.py
@@ -27,7 +27,7 @@ def gen_cmd(cfg_dict, seed):
 
 #cfg to iterate over
 '''
-params=dict(
+params=OrderedDict(
     state_size = [50, 100, 200],
     num_samples = [1500, 5000, 30000],
     batch_size  = [50, 100, 500],
@@ -43,28 +43,27 @@ params=dict(
 )
 '''
 
-params=OrderedDict(
-    state_size = [20, 50, 200],
-    num_samples = [1500],
-    batch_size  = [100],
-    learning_rate = [0.005],
-    grad_norm = [10e2, 10e3],
-    max_output_ops = [5],
-    num_features = [3],
-    train_fn = ["np_mult"],
-    model = ["RNN"],
-    norm = [True]
-)
+params=OrderedDict()
+params['state_size'] = [20, 50, 200]
+params['num_samples'] = [1500]
+params['batch_size']  = [100]
+params['learning_rate'] = [0.005]
+params['grad_norm'] = [10e2, 10e3]
+params['max_output_ops'] = [5]
+params['num_features'] = [3]
+params['train_fn'] = ["np_mult"]
+params['model'] = ["RNN"]
+params['norm'] = [True]
 #cfg which unlinkely is going to be iterated, but still can be configured
 
 #seed
 seed = FLAGS.seed
-
+print (params)
 #for n in range(len(cfg)):
-cfg_dicts = [dict(zip(params, x)) for x in itertools.product(*params.values())]
+cfg_dicts = [OrderedDict(zip(params, x)) for x in itertools.product(*params.values())]
 
 cmds = [gen_cmd(cdict, seed) for cdict in cfg_dicts]
 
 for ind,cmd in enumerate(cmds):
     print("Lnch[" + str(ind+1) +"]: " + cmd)
-    subprocess.Popen(cmd, shell=True, stderr=subprocess.STDOUT)
+    #subprocess.Popen(cmd, shell=True, stderr=subprocess.STDOUT)
