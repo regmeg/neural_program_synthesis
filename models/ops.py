dbf6e132dca591b6409804fd652bf82f1e7ae2fb
Rihards Baranovskis <multiks2200@gmail.com>
2017-08-16 16:26:14 +0100

    added new op

--- models/ops.py
+++ models/ops.py
@@ -1,16 +1,18 @@
 import tensorflow as tf
+import numpy as np
 
 class Operations:
 
-    def __init__(self, cfg):
+    def __init__(self, cfg, mem):
         self.batch_size = cfg['batch_size']
         self.num_features = cfg['num_features']
         self.ops = [self.tf_add, self.tf_multiply, self.tf_stall]
         self.num_of_ops = len(self.ops)
+        self.mem = mem 
 
     #model operations
     #for each reduce based operation, result is reshaped and repadded to fit the model working size num_featuresxbatch_size
-    def tf_add(self, inpt):
+    def tf_add(wself, inpt):
         result = tf.reduce_sum(inpt, axis = 1, name = "tf_add")
         reshape = tf.reshape(result, [self.batch_size, -1], name = "tf_add_reshape")
         pad_res = tf.pad(reshape, [[0,0],[0,self.num_features - 1]], "CONSTANT", name="tf_add_pad")
@@ -26,13 +28,19 @@ class Operations:
     def tf_stall(self, inpt):
         return  inpt
     
-    #get input lenght
+    #get input lenght, asssing all values to ones and then reduce
     def tf_inpt_len(self,inpt):
-        return  inpt
+        inpt = tf.assign(inpt, np.ones(inpt.get_shape()), name="tf_add_assign_ones")
+        result = tf.reduce_sum(inpt, axis = 1, name = "tf_add")
+        reshape = tf.reshape(result, [self.batch_size, -1], name = "tf_add_reshape")
+        pad_res = tf.pad(reshape, [[0,0],[0,self.num_features - 1]], "CONSTANT", name="tf_add_pad")
+        return  pad_res
     
     #divide selected delected numbers
-    def tf_div(self,inpt):
+    def tf_divide(self,inpt):
+        
         return  inpt
+
     #save produced input in temp mem
     def tf_save_inpt(self,inpt):
         return  inpt
